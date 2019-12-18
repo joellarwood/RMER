606a6c9e7d1ae6f2f43912f768b38cfb873bb00a
Joel Larwood <joel.larwood@uq.net.au>
2019-12-18 16:07:41 +1000

    finsh timepoint trait analyses. Begin work on music analysis
    Non normal distributions for music variables. se spearman rank

--- code/RMER_November18_MIR.R
+++ code/RMER_November18_MIR.R
@@ -1,12 +1,12 @@
 # Data Processing November 2018 Prolific Data 
 library(tidyverse)
-library(genius)
+library(spotifyr)
 library(genius)
 
 
 ## Load in Data
 
-Nov18BasicClean <-   read_rds(here::here("data", "RMER_November2018_ProcessedWide.rds"))
+Nov18BasicClean <-read_rds(here::here("data", "RMER_November2018_ProcessedWide.rds"))
 
 # get spotify data
 
@@ -18,9 +18,20 @@ Nov18BasicClean <-   read_rds(here::here("data", "RMER_November2018_ProcessedWid
 track_audio_features <- function(artist, title, type = "track") {
   search_results <- spotifyr::search_spotify(paste(artist, title), type = type)
   
-  track_audio_feats <- spotifyr::get_track_audio_features(search_results$id[[1]]) %>%
-    dplyr::select(-id, -uri, -track_href, -analysis_url)
+  spotify_artist <- dplyr::rename(
+      tibble::enframe(search_results[[1]][[1]][[3]]), 
+    artist_rtn = value)
   
+  spotify_song <- dplyr::rename(
+      tibble::enframe(search_results$name),
+      song_rtn = value)
+  
+  spotify_meta_rtn <- dplyr::select(dplyr::inner_join(spotify_artist, spotify_song), -name) 
+  
+  track_audio_feats <- spotifyr::get_track_audio_features(search_results$id[[1]]) %>%
+    dplyr::select(-uri, -track_href, -analysis_url)
+
+  track_audio_feats <- cbind(spotify_meta_rtn, track_audio_feats)
   return(track_audio_feats)
 }
 
@@ -35,7 +46,11 @@ possible_feats <- possibly(track_audio_features, otherwise = tibble())
 Nov18Features <- Nov18BasicClean %>% 
   mutate(audio_features = map2(sad.song.artist, sad.song, possible_feats)) %>% 
   unnest(cols = c(audio_features)) %>% 
-  as_tibble()
+  as_tibble() %>%  
+  
+
+Nov18Features <- Nov18Features %>% 
+  mutate(song_artist = glue::glue("{song_rtn}_{artist_rtn}"))
 
 #Write CSV
 Nov18Features %>% 
@@ -45,6 +60,25 @@ Nov18Features %>%
 Nov18Features %>% 
   write_rds(here::here("data", "RMER_November2018_Spotify.rds"))
 
+Nov18Lyrics <- Nov18Features %>% 
+  distinct(song_artist, .keep_all = TRUE) %>% 
+  genius::add_genius(artist = artist_rtn, 
+                     title = song_rtn, 
+                     type = "lyrics") %>% 
+  select(song_rtn, artist_rtn, song_artist,  lyrics)
+
+Nov18Features_Lyrics <- Nov18Lyrics %>% 
+  select(song_artist, lyric) %>% 
+  right_join(Nov18Features, by = "song_artist")
+
+
+# Write features and lyrics 
+Nov18Features_Lyrics %>% 
+  write_csv(here::here("data", "RMER_November2018_Spotify_Lyrics.csv"))
+
+#Write Rdata 
+Nov18Features_Lyrics %>% 
+  write_rds(here::here("data", "RMER_November2018_Spotify_Lyrics.rds"))
 
 
 
--- code/RMER_November18_Process.R
+++ code/RMER_November18_Process.R
@@ -1,41 +1,47 @@
 # Data Processing November 2018 Prolific Data 
   
-library(naniar)
+library(visdat)
 library(tidyverse)
 
 ## Load in Data
-
-Nov18Raw <- qualtRics::read_survey(here::here("data", "RMER_November2018.csv")) %>% 
-  replace_with_na_all(condition = ~.x == -99)
-
 testvalues <- c("test", "joel test")
 
-# keep only full responses
-Nov18BasicClean <- Nov18Raw %>%  
+Nov18Raw <- qualtRics::read_survey(here::here("data", "RMER_November2018.csv")) %>% 
+  replace_with_na_all(condition = ~.x == -99) %>% 
   distinct(ProlificID, .keep_all = TRUE) %>%  
   filter(Finished == 1) %>%  
   filter(DistributionChannel == "anonymous") %>%  
   filter(sad.song != testvalues)
+  
+visdat::vis_dat(Nov18Raw)
 
-# Reverse code
-Nov18BasicClean <- Nov18BasicClean %>% 
-  mutate(rrq_6_Orig = rrq_6, 
-         rrq_9_Orig = rrq_9, 
-         rrq_10_Orig = rrq_10, 
-         rrq_6 = 6 - rrq_6, 
-         rrq_9 = 6 - rrq_9, 
-         rrq_10 = 6 - rrq_10)
+reguseupper <- as.numeric(quantile(Nov18Raw$reg.use_1, .66, na.rm = TRUE))
+reguselower <-as.numeric(quantile(Nov18Raw$reg.use_1, .33, na.rm = TRUE))
 
-#score items
-Nov18BasicClean <- Nov18BasicClean %>% 
-  mutate(musebaq = rowSums(dplyr::select(., musebaq_1:musebaq_9)),
-         Baseline = rowSums(dplyr::select(., deq_1_1:deq_1_4)),
-         PostInduction = rowSums(dplyr::select(., deq_2_1: deq_2_4)),
-         PostListening = rowSums(dplyr::select(., deq_3_1: deq_3_4)),
-         rumination = rowMeans(dplyr::select(., rrq_1:rrq_12), na.rm = TRUE), 
-         ResponseId = factor(ResponseId), 
-         age = age + 17
-  )
+# keep only full responses
+Nov18BasicClean <- Nov18Raw %>%  
+  dplyr::mutate(musebaq = rowSums(dplyr::select(., musebaq_1:musebaq_9)),
+                Baseline = rowSums(dplyr::select(., deq_1_1:deq_1_4)),
+                PostInduction = rowSums(dplyr::select(., deq_2_1: deq_2_4)),
+                PostListening = rowSums(dplyr::select(., deq_3_1: deq_3_4)),
+                rumination = rowMeans(dplyr::select(., rrq_1:rrq_12), na.rm = TRUE),
+                musebaq = rowSums(dplyr::select(., musebaq_1:musebaq_9)),
+                ResponseId = factor(ResponseId), 
+                age = age + 17, 
+                reg_use_factor = as_factor(if_else(reg.use_1 >= reguseupper, 
+                                                   "Use", 
+                                                   if_else(reg.use_1 <= reguselower,
+                                                           "Not Use", 
+                                                           "Middle")))) %>%  
+  dplyr::mutate(BSR = mecscale_1 - 1, 
+                Entrainment = mecscale_2 - 1, 
+                Conditioning = mecscale_3 - 1, 
+                Contagion = mecscale_4 - 1, 
+                Imagery = mecscale_5 - 1, 
+                Memory = mecscale_6 - 1, 
+                Expectancy = mecscale_7 - 1, 
+                Appraisal = mecscale_8 - 1) %>% 
+  dplyr::distinct(ProlificID, .keep_all = TRUE)
 
 #export 
 Nov18BasicClean %>% 
